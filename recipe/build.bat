c4ab8c19db3104e04ea735cf35e91c00c0c4d075
Andrew Annex <ama6fy@virginia.edu>
2025-09-23 13:30:18 -0700

    simplified for scikit-build-core

--- recipe/build.bat
+++
@@ -1,3 +0,0 @@
-%PYTHON% setup.py build_ext -I"%LIBRARY_INC%\cspice" -lcspice -L"%LIBRARY_LIB%" 
-%PYTHON% setup.py install --single-version-externally-managed --record record.txt
-if errorlevel 1 exit 1
--- recipe/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-${PYTHON} setup.py build_ext 
-${PYTHON} -m pip install . --no-deps --no-build-isolation -vv
